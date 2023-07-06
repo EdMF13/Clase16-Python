37704d72c1a89c0d1684f0bb2b23e54f10af9516
EduardoPuente <jpuente2379@gmail.com>
2023-07-05 18:26:41 -0600

    Esta es la primer version de mi proyecto

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="mipaquete", 
+    version="1.1",
+    description="Paquete de funciones y variables matematicas",
+    author="Jose Puente",
+    author_mail="jpuente2379@gmail.com",
+    packages=["paquete"]
+)
